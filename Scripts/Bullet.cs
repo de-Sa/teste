e4ac7800f79e865305b0cee97f4c435377c9d3e9
Lucas de Sá <119504411+de-Sa@users.noreply.github.com>
2024-05-27 18:58:28 -0300

    Add files via upload

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+  [SerializeField] private float bulletLifetime = 1f;
+
+  private void Awake() {
+    Destroy(gameObject, bulletLifetime);
+  }
+}
